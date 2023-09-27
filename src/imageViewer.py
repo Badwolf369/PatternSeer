ce5f94d8e26844bf25f8197bf79b635be8de511b
Badwolf926 <sam.wilde369@gmail.com>
2023-09-26 21:55:59 -0700

    Add ability to view all pages of pdf as images

--- src/imageViewer.py
+++ src/imageViewer.py
@@ -2,8 +2,8 @@ from PIL import Image
 
 
 class ImageViewer():
-    def __init__(self, imagePath):
-        self.image = Image.open(imagePath)
+    def __init__(self, image):
+        self.image = image
 
     def view(self):
         self.image.show()
--- src/main.py
+++ src/main.py
@@ -1,14 +1,18 @@
 import sys
 from imageViewer import ImageViewer
+import pypdfium2 as pdfium
 
 
-def main(imagePath):
-    display = ImageViewer(imagePath)
-    display.view()
+def main(pdfPath):
+    document = pdfium.PdfDocument(pdfPath)
+    for page in document:
+        pageImage = page.render().to_pil()
+        display = ImageViewer(pageImage)
+        display.view()
 
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        print("Error: please input a path to an image")
+        print("Error: please give the path to a pdf file to work with")
         exit()
     main(sys.argv[1])
