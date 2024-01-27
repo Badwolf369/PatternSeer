9c20de21042ad1f9521e4f071b0a52277cba3519
Badwolf926 <sam.wilde369@gmail.com>
2024-01-27 11:42:37 -0800

    remove irrelivant code

--- PatternSeer.PatternSeer/src/Program.cs
+++ PatternSeer.PatternSeer/src/Program.cs
@@ -3,6 +3,7 @@ using Emgu.CV;
 using Emgu.CV.CvEnum;
 using SkiaSharp;
 using Avalonia.ReactiveUI;
+using Emgu.CV.Structure;
 
 namespace PatternSeer;
 
@@ -10,46 +11,6 @@ namespace PatternSeer;
 /// Main entry point for the software.
 /// </summary>
 class Program {
-    /// <summary>
-    /// Take user input from the command line, sterilize it, and give it to the program.
-    /// </summary>
-    /// <param name="args">User's command line input</param>
-    static void Mains(string[] args) {
-        string PatternAddress;
-        while(true) {
-            Console.WriteLine("Please enter the path of a valid PDF File:");
-            PatternAddress = Console.ReadLine();
-            if (Util.validPdfFile(PatternAddress)) {
-                Console.WriteLine($"Importing {PatternAddress}");
-                break;
-            }
-            else {
-                Console.WriteLine($"\"{PatternAddress}\" does not exist or is not a PDF not exist.");
-            }
-        }
-
-        List<Mat> pdfCvImages = new List<Mat>();
-        byte[] pdfBytes = File.ReadAllBytes(PatternAddress);
-        string pdf64String = Convert.ToBase64String(pdfBytes);
-        var pdfSKBmps = PDFtoImage.Conversion.ToImages(pdf64String)
-            .Cast<SKBitmap>().ToList();
-        for (int page = 0; page < pdfSKBmps.Count; page++) {
-            using (MemoryStream imageStream = new MemoryStream()) {
-                pdfSKBmps[page].Encode(imageStream, SKEncodedImageFormat.Png, 100);
-                pdfCvImages.Add(new Mat());
-                CvInvoke.Imdecode(imageStream.ToArray(), ImreadModes.Color, pdfCvImages[page]);
-            }
-            Console.WriteLine($"Sucessfully loaded page {page} of pattern");
-            //? \/ image debugging \/
-            // Mat cvDebug = new Mat();
-            // CvInvoke.ResizeForFrame(pdfCvImages[page], cvDebug, new Size(480, 640));
-            // CvInvoke.Imshow($"Page {page}", cvDebug);
-            //? /\ image debugging /\
-        }
-        CvInvoke.WaitKey();
-        CvInvoke.DestroyAllWindows();
-    }
-
     /// <summary>
     /// Configures options for the creation of an Avalonia app.
     /// </summary>
